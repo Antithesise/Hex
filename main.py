36d65053771c45206c0ea7f5cc68ca15482dbb00
good-coder-bad-boy <69414897+good-coder-bad-boy@users.noreply.github.com>
2021-09-24 15:14:27 +1000

    runtime

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-#system("python3 Hex/Lib/Aquarium.py")
-#system("python3 Hex/Lib/Errors.py")
-#system("python3 Hex/Lib/Hex.py")
-#system("python3 Hex/Lib/HexREPL.py")
-#system("python3 Hex/Lib/Htime.py")
-#system("python3 Hex/Lib/UnrealTimeClock.py")
+from os import system
+
+# system("python3 Hex/Lib/Aquarium.py")
+# system("python3 Hex/Lib/Errors.py")
+# system("python3 Hex/Lib/Hex.py")
+# system("python3 Hex/Lib/HexREPL.py")
+# system("python3 Hex/Lib/Htime.py")
+# system("python3 Hex/Lib/UnrealTimeClock.py")
+# system("bash Hex/tests/HelloWorld.sh")
